0b760ba77d5abb065ac7843bac31c6576cf4f261
kittijaaa <kittipat@ku.th>
2021-03-15 17:59:32 +0700

    1

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,m,k;
+    scanf("%d",&n);
+    char x[n][1000];
+    int y[n],mi[n];
+    for(i=0; i<n; i++)
+        scanf(" %[^\n]s",x[i]);
+    for(j=0; j<26; j++)
+    {
+        for(i=0; i<n; i++)
+        {
+            if((x[i][0]==65+j)||(x[i][0]==97+j))
+                printf("%s\n",x[i]);
+        }
+    }
+    return 0 ;
+}
